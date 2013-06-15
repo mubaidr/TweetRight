0e31863ddbc80f65df4913821811c2b80fda4614
Arpit Kumar <triwin2020@gmail.com>
2013-06-16 03:47:58 +0530

    v0.95
    code optimization

--- tweetright.js
+++ tweetright.js
@@ -1,23 +1,23 @@
 // TweetRight Chrome extension by @ArpitNext
-// Homepage: http://blog.arpitnext.com/tweetright
+// Homepage: http://browserfame.com/tweetright
 
 // onClicked callback function.
 function onClickHandler(info, tab) {
   if (info.menuItemId == "selection") {
-    var postUrl = 'https://twitter.com/intent/tweet?text="SELTEXT" &url='+info.pageUrl;
-	chrome.windows.create({"url":postUrl.replace("SELTEXT", encodeURIComponent(info.selectionText)), "type":"popup", "height":300,"width":600});
+    var postUrl = 'https://twitter.com/intent/tweet?text='+encodeURIComponent('"'+info.selectionText+'"')+'&url='+info.pageUrl;
+    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
   }
   if (info.menuItemId == "page") {
-    var postUrl = "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url=SELTEXT";
-	chrome.windows.create({"url":postUrl.replace("SELTEXT", encodeURIComponent(info.pageUrl)), "type":"popup", "height":300,"width":600});
+    var postUrl = "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url="+encodeURIComponent(info.pageUrl);
+    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
   }
   if (info.menuItemId == "link") {
-    var postUrl = "https://twitter.com/intent/tweet?text=[link] &url=SELTEXT";
-	chrome.windows.create({"url":postUrl.replace("SELTEXT", encodeURIComponent(info.linkUrl)), "type":"popup", "height":300,"width":600});
+    var postUrl = "https://twitter.com/intent/tweet?text=[link] &url="+encodeURIComponent(info.linkUrl);
+    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
   }
   if (info.menuItemId == "image") {
-    var postUrl = "https://twitter.com/intent/tweet?text=[image] &url=SELTEXT";
-	chrome.windows.create({"url":postUrl.replace("SELTEXT", encodeURIComponent(info.srcUrl)), "type":"popup", "height":300,"width":600});
+    var postUrl = "https://twitter.com/intent/tweet?text=[image] &url="+encodeURIComponent(info.srcUrl);
+    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
   }
 };
 
@@ -31,11 +31,11 @@ chrome.runtime.onInstalled.addListener(function() {
     var title = "Tweet " + context;
     var id = chrome.contextMenus.create({"title": title, "contexts":[context],
                                          "id": context});
-										 }
+  }
 });
 
 // for toolbar button  
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var postUrl = "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+" &url=SELTEXT";
-  chrome.windows.create({"url":postUrl.replace("SELTEXT", tab.url), "type":"popup", "height":300,"width":600, "top": 100, "left":100})
+  var postUrl = "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url="+encodeURIComponent(tab.url);
+  chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600, "top": 100, "left":100})
 });
