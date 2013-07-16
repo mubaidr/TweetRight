3f0643c136f53b7455e8faecbeb21a7529d0d478
mubaidr <mubaidr@gmail.com>
2013-07-16 17:55:09 +0500

    v1.0
    -Changed persistent setting to true
    -Open new tab instead of popup

--- tweetright.js
+++ tweetright.js
@@ -1,23 +1,23 @@
-// TweetRight Chrome extension by @ArpitNext
+// TweetRight Chrome extension by @mubaidr
 // Homepage: https://github.com/mubaidr/TweetRight+/
 
 // onClicked callback function.
 function onClickHandler(info, tab) {
   if (info.menuItemId == "selection") {
-    var postUrl = 'https://twitter.com/intent/tweet?text='+encodeURIComponent('"'+info.selectionText+'"')+'&url='+info.pageUrl;
-    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
+    var postUrl = 'https://twitter.com/intent/tweet?text='+encodeURIComponent('"'+info.selectionText+'"');
+    chrome.tabs.create({"url":postUrl});
   }
   if (info.menuItemId == "page") {
     var postUrl = "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url="+encodeURIComponent(info.pageUrl);
-    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
+    chrome.tabs.create({"url":postUrl});
   }
   if (info.menuItemId == "link") {
-    var postUrl = "https://twitter.com/intent/tweet?text=[link] &url="+encodeURIComponent(info.linkUrl);
-    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
+    var postUrl = "https://twitter.com/intent/tweet?text=&url="+encodeURIComponent(info.linkUrl);
+    chrome.tabs.create({"url":postUrl});
   }
   if (info.menuItemId == "image") {
-    var postUrl = "https://twitter.com/intent/tweet?text=[image] &url="+encodeURIComponent(info.srcUrl);
-    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
+    var postUrl = "https://twitter.com/intent/tweet?text=&url="+encodeURIComponent(info.srcUrl);
+    chrome.tabs.create({"url":postUrl});
   }
 };
 
@@ -27,12 +27,6 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 var contexts = ["page","selection","link","image"];
 for (var i = 0; i < contexts.length; i++) {
   var context = contexts[i];
-  var title = "Tweet this " + context;
+  var title = "Tweet " + context;
   var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": context});
 }
-
-// for toolbar button  
-chrome.browserAction.onClicked.addListener(function(tab) {
-  var postUrl = "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url="+encodeURIComponent(tab.url);
-  chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600, "top": 100, "left":100})
-});
