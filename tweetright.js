00212c51b8883f2f72f1400b2aee2bad70abd7e9
arpitnext.techraga@gmail.com <>
2014-01-14 23:52:55 +0000

    code optimization

--- tweetright.js
+++ tweetright.js
@@ -1,23 +1,28 @@
-// TweetRight Chrome extension by @ArpitNext
-// Homepage: http://browserfame.com/tweetright
+// 'Share on Twitter' Chrome extension by @ArpitNext
+// Homepage: http://browsernative.com/extensions/
+
+
+var Tweet = function(postUrl) {
+  chrome.windows.create({"url":postUrl, "type":"popup", "height":450,"width":600, "top": 100, "left":100});
+}
 
 // onClicked callback function.
 function onClickHandler(info, tab) {
   if (info.menuItemId == "selection") {
     var postUrl = 'https://twitter.com/intent/tweet?text='+encodeURIComponent('"'+info.selectionText+'"')+'&url='+info.pageUrl;
-    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
+    Tweet(postUrl);
   }
   if (info.menuItemId == "page") {
     var postUrl = "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url="+encodeURIComponent(info.pageUrl);
-    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
+    Tweet(postUrl);
   }
   if (info.menuItemId == "link") {
     var postUrl = "https://twitter.com/intent/tweet?text=[link] &url="+encodeURIComponent(info.linkUrl);
-    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
+    Tweet(postUrl);
   }
   if (info.menuItemId == "image") {
     var postUrl = "https://twitter.com/intent/tweet?text=[image] &url="+encodeURIComponent(info.srcUrl);
-    chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600});
+    Tweet(postUrl);
   }
 };
 
@@ -34,5 +39,23 @@ for (var i = 0; i < contexts.length; i++) {
 // for toolbar button  
 chrome.browserAction.onClicked.addListener(function(tab) {
   var postUrl = "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url="+encodeURIComponent(tab.url);
-  chrome.windows.create({"url":postUrl, "type":"popup", "height":300,"width":600, "top": 100, "left":100})
+  Tweet(postUrl);
 });
+
+
+  
+// run the first-run page
+
+chrome.runtime.onInstalled.addListener( function(details) {
+  if(details.reason == "install"){
+    chrome.tabs.create({url: "firstrun.html"});
+  }
+  if(details.reason == "update"){
+    chrome.tabs.create({url:"update.html"});
+  }
+});
+  
+chrome.runtime.setUninstallUrl("http://browsernative.com/feedback/?i=tr");
+  
+  
+  
