986bd1b49c075e141279123125b847c35f6f76ba
Arpit Kumar <arpitnext@gmail.com>
2016-08-17 23:47:54 +0530

    Code sync with the latest version on CWS

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 "Share on Twitter" (earlier, "TweetRight") is a Chrome extension for Twitter users.
 
-For more information visit homepage: http://browsernative.com/share-on-twitter-chrome-extension/
+For more information visit homepage: https://browsernative.com/share-on-twitter-chrome-extension/
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// 'Share on Twitter' Chrome extension by @ArpitNext
-// Homepage: http://browsernative.com/share-on-twitter-chrome-extension/
+// 'Share on Twitter' Chrome extension by BrowserNative
+// Homepage: https://browsernative.com/share-on-twitter-chrome-extension/
 
 
 // onClicked callback function.
@@ -24,26 +24,30 @@ function onClickHandler(info, tab) {
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
-// Setting up context menu items.
-var contexts = ["page","selection","link","image"];
-for (var i = 0; i < contexts.length; i++) {
-  var context = contexts[i];
-  var title = "Tweet " + context;
-  var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": context});
-}
 
-// for toolbar button  
+
+// for toolbar button
 chrome.browserAction.onClicked.addListener(function(tab) {
   var postUrl = "https://twitter.com/intent/tweet?text="+encodeURIComponent(tab.title)+"&url="+encodeURIComponent(tab.url);
   chrome.windows.create({"url":postUrl, "type":"popup", "height":450,"width":600, "top": 100, "left":100, incognito: tab.incognito});
 });
 
 
-  
-// run the first-run page
 
 chrome.runtime.onInstalled.addListener( function(details) {
+
+  // Setting up context menu items.
+  var contexts = ["page","selection","link","image"];
+  for (var i = 0; i < contexts.length; i++) {
+    var context = contexts[i];
+    var title = "Tweet " + context;
+    var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": context});
+  }
+
+  
   if(details.reason == "install"){
-    chrome.tabs.create({url: "http://browsernative.com/share-on-twitter-chrome-extension/"});
+    chrome.tabs.create({url: "https://browsernative.com/share-on-twitter-chrome-extension/"});
   }
 });
+
+chrome.runtime.setUninstallURL("https://browsernative.com/static/tr.html");
